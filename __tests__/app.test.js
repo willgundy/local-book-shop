2afa8e53ba634278339fd43ad1d11685b9b8722e
Will Gundy <willgunderson503@gmail.com>
2022-06-14 10:32:12 -0700

    wrote test for getting a list of books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,21 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('book routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  //test for getting a list of books
+  it('should return a list of books w/o nested authors', async () => {
+    const res = await request(app).get('/books');
+    expect(res.status).toBe(200);
+    expect(res.body.length).toEqual(2);
+    const mockingbird = res.body.find((book) => book.title === 'To Kill A Mockingbird');
+    expect(mockingbird).toHaveProperty('title', 'To Kill A Mockingbird');
+    expect(mockingbird).toHaveProperty('released', 1960);
   });
+
   afterAll(() => {
     pool.end();
   });
