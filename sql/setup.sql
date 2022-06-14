0c62dcd77de4cd43e8fca5ddf1e0c78d25be2577
Will Gundy <willgunderson503@gmail.com>
2022-06-14 10:15:48 -0700

    setup books model

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,30 +1,31 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS books;
-DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS authors_to_books;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS books;
+
 
 CREATE TABLE books (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   title VARCHAR NOT NULL,
   released INT NOT NULL
-)
+);
 
 CREATE TABLE authors (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   dob VARCHAR,
   pob VARCHAR,
   name VARCHAR NOT NULL
-)
+);
 
 CREATE TABLE authors_to_books (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
   book_id BIGINT,
   author_id BIGINT,
   FOREIGN KEY (book_id) REFERENCES books(id),
   FOREIGN KEY (author_id) REFERENCES authors(id)
-)
+);
 
 INSERT INTO books (title, released)
 VALUES
@@ -36,7 +37,7 @@ VALUES
   ('04/28/1926', 'Monroeville, AL', 'Nelle Harper Lee'), 
   ('08/01/1819', 'New York, NY', 'Herman Melville');
 
-INSERT INTO books (book_id, author_id)
+INSERT INTO authors_to_books (book_id, author_id)
 VALUES
   (1, 1), 
   (2, 2);
