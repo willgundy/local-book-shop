59dfc7136ae115d06d1a9fb7e6a415bc29007931
Will Gundy <willgunderson503@gmail.com>
2022-06-14 10:52:31 -0700

    added test for single book, still don't have authors test for single book

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -8,7 +8,6 @@ describe('book routes', () => {
     return setup(pool);
   });
 
-  //test for getting a list of books
   it('should return a list of books w/o nested authors', async () => {
     const res = await request(app).get('/books');
     expect(res.status).toBe(200);
@@ -16,6 +15,14 @@ describe('book routes', () => {
     const mockingbird = res.body.find((book) => book.title === 'To Kill A Mockingbird');
     expect(mockingbird).toHaveProperty('title', 'To Kill A Mockingbird');
     expect(mockingbird).toHaveProperty('released', 1960);
+  });
+
+  //need to write in test for nested authors
+  it('should return a single books details', async () => {
+    const res = await request(app).get('/books/1');
+    expect(res.status).toBe(200);
+    expect(res.body.title).toEqual('To Kill A Mockingbird');
+    expect(res.body.released).toEqual(1960);
   });
 
   afterAll(() => {
